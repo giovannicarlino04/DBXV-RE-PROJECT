981637c214b054198590c77ec30d6d63a69614f4
Giovanni Carlino <97813246+giovannicarlino04@users.noreply.github.com>
2023-05-02 22:01:37 +0200

    Update

---
+++ XVTool/XVTool/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace XVTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            installModToolStripMenuItem = new ToolStripMenuItem();
+            uninstallModToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            toolsToolStripMenuItem = new ToolStripMenuItem();
+            clearInstallationToolStripMenuItem = new ToolStripMenuItem();
+            listBox1 = new ListBox();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, toolsToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { installModToolStripMenuItem, uninstallModToolStripMenuItem, toolStripSeparator1, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // installModToolStripMenuItem
+            // 
+            installModToolStripMenuItem.Name = "installModToolStripMenuItem";
+            installModToolStripMenuItem.Size = new Size(148, 22);
+            installModToolStripMenuItem.Text = "Install Mod";
+            installModToolStripMenuItem.Click += installModToolStripMenuItem_Click;
+            // 
+            // uninstallModToolStripMenuItem
+            // 
+            uninstallModToolStripMenuItem.Name = "uninstallModToolStripMenuItem";
+            uninstallModToolStripMenuItem.Size = new Size(148, 22);
+            uninstallModToolStripMenuItem.Text = "Uninstall Mod";
+            uninstallModToolStripMenuItem.Click += btnUninstallMod_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(145, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(148, 22);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // toolsToolStripMenuItem
+            // 
+            toolsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { clearInstallationToolStripMenuItem });
+            toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            toolsToolStripMenuItem.Size = new Size(46, 20);
+            toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // clearInstallationToolStripMenuItem
+            // 
+            clearInstallationToolStripMenuItem.Name = "clearInstallationToolStripMenuItem";
+            clearInstallationToolStripMenuItem.Size = new Size(162, 22);
+            clearInstallationToolStripMenuItem.Text = "Clear Installation";
+            clearInstallationToolStripMenuItem.Click += clearInstallationToolStripMenuItem_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.Dock = DockStyle.Fill;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(0, 24);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(800, 426);
+            listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "XVTool";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem installModToolStripMenuItem;
+        private ToolStripMenuItem uninstallModToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ListBox listBox1;
+        private ToolStripMenuItem toolsToolStripMenuItem;
+        private ToolStripMenuItem clearInstallationToolStripMenuItem;
+    }
+}
